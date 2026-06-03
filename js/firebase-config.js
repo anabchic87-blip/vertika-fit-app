7d0736f38b5c30146e3241b4b869f2f0701088f4
anabchic87-blip <anabchic87@hotmail.com>
2026-06-03 18:04:41 +0200

    Delete js directory

--- js/firebase-config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyA8qlF6AfXAlLCLvrooG8IY0fBSHWCpfEU",
-  authDomain: "vertika-fit-app-c361a.firebaseapp.com",
-  projectId: "vertika-fit-app-c361a",
-  storageBucket: "vertika-fit-app-c361a.firebasestorage.app",
-  messagingSenderId: "746569602423",
-  appId: "1:746569602423:web:b7713b5746cee4fdd63d72"
-};
