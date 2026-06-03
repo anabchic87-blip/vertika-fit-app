e1698dbb60d22acb18ebe0878fb61418b3e2b18d
anabchic87-blip <anabchic87@hotmail.com>
2026-06-03 18:20:01 +0200

    Update workout-page.js

--- js/workout-page.js
+++ js/workout-page.js
@@ -3,6 +3,14 @@ function workoutKey() {
   const today = new Date().toISOString().slice(0, 10);
   return "vertikaWorkoutPage_" + slug + "_" + today;
 }
+export const firebaseConfig = {
+  apiKey: "AIzaSyA8qlF6AfXAlLCLvrooG8IY0fBSHWCpfEU",
+  authDomain: "vertika-fit-app-c361a.firebaseapp.com",
+  projectId: "vertika-fit-app-c361a",
+  storageBucket: "vertika-fit-app-c361a.firebasestorage.app",
+  messagingSenderId: "746569602423",
+  appId: "1:746569602423:web:b7713b5746cee4fdd63d72"
+};
 
 function loadChecks() {
   const saved = JSON.parse(localStorage.getItem(workoutKey()) || "{}");
