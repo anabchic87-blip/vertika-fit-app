8392df7f5f5d25d029d86560d6dfd40101481111
anabchic87-blip <anabchic87@hotmail.com>
2026-06-04 09:10:53 +0200

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,6 +0,0 @@
-const CACHE_NAME = "vertika-fit-19-clean-index";
-self.addEventListener("install", event => self.skipWaiting());
-self.addEventListener("activate", event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(() => self.clients.claim()));
-});
-self.addEventListener("fetch", event => event.respondWith(fetch(event.request)));
