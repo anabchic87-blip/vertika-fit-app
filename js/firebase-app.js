7337ca983c515d9e26a3f16dd49eee0cbfdc8ecc
anabchic87-blip <anabchic87@hotmail.com>
2026-06-04 18:05:22 +0200

    Delete js directory

--- js/firebase-app.js
+++
@@ -1,1 +0,0 @@
-// VÉRTIKA FIT 16 local: firebase-app.js desactivado. No se usa Firebase.
--- js/mobile-simple.js
+++
@@ -1,5 +0,0 @@
-/* VÉRTIKA FIT 20
-   Menú móvil inferior y botones laterales desactivados.
-   La navegación queda limpia desde el índice/sidebar.
-*/
-console.log("VÉRTIKA FIT 20: navegación móvil inferior desactivada.");
